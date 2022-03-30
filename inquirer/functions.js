24f39dbe6fa7c3ba70f5d139ca03503a459d82d7
Michael Marlowe <marlowemich@gmail.com>
2022-03-30 10:32:00 -0400

    added employeeUpdate()

--- inquirer/functions.js
+++ inquirer/functions.js
@@ -1,15 +1,22 @@
-const inquirer = require('inquirer');
+const inquirer = require("inquirer");
 const cTable = require("console.table");
 const db = require("../config/connection");
-const { end } = require('../config/connection');
-const { get } = require('express/lib/request');
-  const navList = {
-    type: 'list',
-    message: 'What would you like to do?',
-    name: 'choice',
-    choices: ['View All Employees', 'Add Employee', 'Update An Employee',
-                'View All Roles', 'Add Role', 
-                'View All Departments', 'Add Department', 'EXIT'],
+const { end } = require("../config/connection");
+const { get } = require("express/lib/request");
+const navList = {
+  type: "list",
+  message: "What would you like to do?",
+  name: "choice",
+  choices: [
+    "View All Employees",
+    "Add Employee",
+    "Update An Employee",
+    "View All Roles",
+    "Add Role",
+    "View All Departments",
+    "Add Department",
+    "EXIT",
+  ],
 };
 
 const deptArr = [];
@@ -18,167 +25,192 @@ const empArr = [];
 
 //BEGINNING OF ASYNC INQUIRER.PROMPT FUNCTIONS
 async function navFunc() {
-    const navData = await inquirer.prompt(navList);
-    switch (navData.choice) {
-      case "View All Employees":
-        viewEmployee();
-        break;
-      case "Add Employee":
-        newEmployee();
-        break;
-      case "Update an Employee":
-        updateEmployee();
-        break;
-      case "View All Roles":
-        viewRole();
-        break;
-      case "Add Role":
-        newRole();
-        break;
-      case "View All Departments":
-        viewDept();
-        break;
-      case "Add Department":
-        newDept();
-        break;
-      case "EXIT":
-        return;
-    };
-  
-    function viewEmployee() {
-        db.query(`SELECT * FROM employee`, (err, results) => {
-          console.table(results);
-          return navFunc();
-        });
-      }
+  const navData = await inquirer.prompt(navList);
+  switch (navData.choice) {
+    case "View All Employees":
+      viewEmployee();
+      break;
+    case "Add Employee":
+      newEmployee();
+      break;
+    case "Update an Employee":
+      updateEmployee();
+      break;
+    case "View All Roles":
+      viewRole();
+      break;
+    case "Add Role":
+      newRole();
+      break;
+    case "View All Departments":
+      viewDept();
+      break;
+    case "Add Department":
+      newDept();
+      break;
+    case "EXIT":
+      return;
+  }
 
-      function viewRole() {
-        db.query(`SELECT * FROM deptRole`, (err, results) => {
-          console.table(results);
-          return navFunc();
-        });
-      }
+  function viewEmployee() {
+    db.query(`SELECT * FROM employee`, (err, results) => {
+      console.table(results);
+      return navFunc();
+    });
+  }
 
-      function viewDept() {
-        db.query(`SELECT * FROM department`, (err, results) => {
-         console.table(results);
-          return navFunc();
-        });
-      }
+  function viewRole() {
+    db.query(`SELECT * FROM deptRole`, (err, results) => {
+      console.table(results);
+      return navFunc();
+    });
+  }
 
-      
-  
-   async function newEmployee() {
-    const employeeSet = [
+  function viewDept() {
+    db.query(`SELECT * FROM department`, (err, results) => {
+      console.table(results);
+      return navFunc();
+    });
+  }
+
+  async function updateEmployee() {
+    const employeeUpdate = [
       {
-          type: 'input',
-          message: "What is the employee's first name?",
-          name: "first_name"
-      },
-      {
-          type: 'input',
-          message: "What is the employee's last name?",
-          name: "last_name"
+          type: 'number',
+          message: 'What is the ID of the employee you would like to update?',
+          name: 'employeeID'
       },
       {
           type: 'number',
-          message: "What is the role ID of this employee?",
+          message: "What is the ID of the new role of the employee?",
           name: 'role'
       },
-      {
-        type: 'number',
-        message: "What is the Manager's ID over this employee?",
-        name: "manager"
-      },
       ];
-      const employeeData = await inquirer.prompt(employeeSet);
-      empArr.push(employeeData);
-      db.query(
-        `INSERT INTO employee (first_name, last_name, role_id, manager_id) 
+    const updateInfo = await inquirer.prompt(employeeUpdate);
+    db.query(`UPDATE employee SET role_id = ${updateInfo.role} WHERE employee.id = ${updateInfo.employeeID}`,
+    (err) => {
+      if (err) {
+        console.error(
+          (`\n Update did not process.`)
+        );
+        return navFunc();
+      } else {
+        console.log(`\n Employee role changed successfully!`)
+      };
+    });
+    return navFunc();
+  };
+
+  async function newEmployee() {
+    const employeeSet = [
+      {
+        type: "input",
+        message: "What is the employee's first name?",
+        name: "first_name",
+      },
+      {
+        type: "input",
+        message: "What is the employee's last name?",
+        name: "last_name",
+      },
+      {
+        type: "number",
+        message: "What is the role ID of this employee?",
+        name: "role",
+      },
+      {
+        type: "number",
+        message: "What is the Manager's ID over this employee?",
+        name: "manager",
+      },
+    ];
+    const employeeData = await inquirer.prompt(employeeSet);
+    empArr.push(employeeData);
+    db.query(
+      `INSERT INTO employee (first_name, last_name, role_id, manager_id) 
           VALUES 
           ('${employeeData.first_name}', '${employeeData.last_name}', 
             ${employeeData.role}, ${employeeData.manager})`,
-        (err) => {
-          if (err) {
-            console.error(
-              `\n ${employeeData.first_name} ${employeeData.last_name} is not a valid entry or already exists.`
-            );
-            return navFunc();
-          } else {
-            console.log(
-              `\n ${employeeData.first_name} ${employeeData.last_name} has been added to the Employee database!`
-            );
-          }
+      (err) => {
+        if (err) {
+          console.error(
+            `\n ${employeeData.first_name} ${employeeData.last_name} is not a valid entry or already exists.`
+          );
+          return navFunc();
+        } else {
+          console.log(
+            `\n ${employeeData.first_name} ${employeeData.last_name} has been added to the Employee database!`
+          );
         }
-      );
-      return navFunc();
-    }
-  
- async function newRole() {
-                   const roleSet =
-                   [
-                    {
-                        type: 'input',
-                        message: "What is the name of this role?",
-                        name: 'roleName'
-                    },
-                    {
-                        type: 'number',
-                        message: 'What is the salary of this role?',
-                        name: 'roleSalary'
-                    },
-                    {
-                      type: 'number',
-                      message: "What is the department ID of this role?",
-                      name: "roleDept"
-                    }
-                    ]
-        const roleData = await inquirer.prompt(roleSet);
-        roleArr.push(roleData)
-      db.query(
-        `INSERT INTO deptRole (title, salary, department_id) VALUES 
+      }
+    );
+    return navFunc();
+  }
+
+  async function newRole() {
+    const roleSet = [
+      {
+        type: "input",
+        message: "What is the name of this role?",
+        name: "roleName",
+      },
+      {
+        type: "number",
+        message: "What is the salary of this role?",
+        name: "roleSalary",
+      },
+      {
+        type: "number",
+        message: "What is the department ID of this role?",
+        name: "roleDept",
+      },
+    ];
+    const roleData = await inquirer.prompt(roleSet);
+    roleArr.push(roleData);
+    db.query(
+      `INSERT INTO deptRole (title, salary, department_id) VALUES 
           ('${roleData.roleName}', ${roleData.roleSalary}, ${roleData.roleDept})`,
-        (err) => {
-          if (err) {
-            console.error(
-              `\n ${roleData.roleName} is not a valid entry or already exists.`
-            );
-            return navFunc();
-          } else {
-            console.log(
-              `\n ${roleData.roleName} has been added to the Role database!`
-            );
-          }
+      (err) => {
+        if (err) {
+          console.error(
+            `\n ${roleData.roleName} is not a valid entry or already exists.`
+          );
+          return navFunc();
+        } else {
+          console.log(
+            `\n ${roleData.roleName} has been added to the Role database!`
+          );
         }
-      );
-      return navFunc();
-    }
+      }
+    );
+    return navFunc();
+  }
 
-    async function newDept() {
-        const deptSet = {
-            type: 'input',
-            message: "What is the name of this department?",
-            name: "deptName"
-        };
-      const deptData = await inquirer.prompt(deptSet);
-      deptArr.push(deptData);
-      db.query(
-        `INSERT INTO department (dept_name) VALUES ('${deptData.deptName}')`,
-        (err) => {
-          if (err) {
-            console.error(
-              `\n ${deptData.deptName} is not a valid entry or already exists.`
-            );
-            return navFunc();
-          } else {
-            console.log(
-              `\n ${deptData.deptName} has been added to the Department database!`
-            );
-          }
+  async function newDept() {
+    const deptSet = {
+      type: "input",
+      message: "What is the name of this department?",
+      name: "deptName",
+    };
+    const deptData = await inquirer.prompt(deptSet);
+    deptArr.push(deptData);
+    db.query(
+      `INSERT INTO department (dept_name) VALUES ('${deptData.deptName}')`,
+      (err) => {
+        if (err) {
+          console.error(
+            `\n ${deptData.deptName} is not a valid entry or already exists.`
+          );
+          return navFunc();
+        } else {
+          console.log(
+            `\n ${deptData.deptName} has been added to the Department database!`
+          );
         }
-      );
-      return navFunc();
-    }
-  } //END OF ASYNC INQUIRER.PROMPT FUNCTIONS
+      }
+    );
+    return navFunc();
+  }
+} //END OF ASYNC INQUIRER.PROMPT FUNCTIONS
 
-  module.exports = navFunc;
+module.exports = navFunc;
